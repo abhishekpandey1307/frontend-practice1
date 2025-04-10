c726d09dae00af98cdcfa0dffed2b63c73cefc9d
Abhishek Santosh Pandey <write2abhishek4400@gmail.com>
2025-04-10 21:58:50 +0530

    chore: added timeout to hide success message

--- frontend-practice-files/contact-form/script.js
+++ frontend-practice-files/contact-form/script.js
@@ -1,1 +1,9 @@
-<!-- script.js for contact-form -->
+const form = document.getElementById('contactForm');
+const status = document.querySelector('.form-status');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  status.textContent = 'Message sent!';
+  form.reset();
+});
+
